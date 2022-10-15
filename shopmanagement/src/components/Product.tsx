b45ca9477846d9d96fffe2925bd39c29515984b5
Maxim Derboven <maxim.derboven@student.kdg.be>
2022-10-15 12:16:10 +0200

    fixed error and warning when querying

--- shopmanagement/src/components/Product.tsx
+++ shopmanagement/src/components/Product.tsx
@@ -1,5 +1,6 @@
 import { Product as ProductModel } from "../model/Product";
 import { useMutation, useQuery } from "@tanstack/react-query";
+import {Alert, CircularProgress, Fab} from "@mui/material";
 import {useParams} from "react-router-dom";
 import { useProduct } from "../hooks/useProduct";
 import { useState } from "react";
@@ -7,27 +8,23 @@ import { useState } from "react";
 export function Product() {
   const {id} = useParams();
   const {isLoading, isError, product} = useProduct(id!);
-
   if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (isError) {
-    return <div>Dikke error</div>;
-  }
-
-    if (product == null) {
-        return <div>Product not found</div>;
-    }
-
+    return <CircularProgress />;
+  } else 
+  if (!product) {
+    return <Alert severity="error">Product does not exist</Alert>;
+   } else if (isError) {
+    return <Alert severity="error">Product could not be loaded</Alert>;
+  } else {
     return (
-        <div>
-            <h1>Product</h1>
-            <ul>
-                <li>
-                    {product.name} - {product.price}
-                </li>
-            </ul>
-        </div>
+      <div>
+        <h1>Product</h1>
+        <ul>
+          <li>
+            {product.name} - {product.price}
+          </li>
+        </ul>
+      </div>
     );
+  }
 };
